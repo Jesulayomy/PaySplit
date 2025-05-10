0333a047f0e3fcaf5fdc22fcec950a82c5a23dd5
Jesulayomy <jesulayomi081@gmail.com>
2025-05-10 12:41:57 -0400

    [Image]: Implement disk and memory upload methods

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,14 +1,23 @@
 const multer = require("multer");
 const path = require("path");
 
-module.exports = multer({
-  storage: multer.diskStorage({}),
-  fileFilter: (req, file, cb) => {
-    let ext = path.extname(file.originalname).toLowerCase();
-    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
-      cb(new Error("File type is not supported"), false);
-      return;
-    }
-    cb(null, true);
-  },
-});
+
+const fileFilter = (req, file, cb) => {
+  let ext = path.extname(file.originalname).toLowerCase();
+  if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+    cb(new Error("File type is not supported"), false);
+    return;
+  }
+  cb(null, true);
+}
+
+module.exports = { 
+  diskUpload: multer({
+    storage: multer.diskStorage({}),
+    fileFilter
+  }),
+  memoryUpload: multer({
+    storage: multer.memoryStorage(),
+    fileFilter,
+  })
+};
